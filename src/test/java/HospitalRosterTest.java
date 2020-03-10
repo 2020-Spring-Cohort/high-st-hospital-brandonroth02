123d8bbe65f419885593d864a02a6ff46434f499
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-10 12:13:19 -0400

    started Roster test

---
+++ src/test/java/HospitalRosterTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class HospitalRosterTest {
+    private HospitalRoster underTest;
+    private Employee testEmployee;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new HospitalRoster();
+        testEmployee = new Employee("TestEmployee", 100);
+        underTest.hire(testEmployee);
+    }
+
+    @Test
+    public  void  listOfEmployees(){
+        ArrayList<Employee> retrievedEmployee = underTest.retrieveEmployeeList();
+        assertEquals( 1, retrievedEmployee.size());
+    }
+    @Test
+    public void shouldPayAllEmployees(){
+        underTest.payEmployees();
+        ArrayList<Employee> retrievedEmployees = underTest.retrieveEmployeeList();
+        assertTrue(retrievedEmployees.get(0).getSalaryAmmount());
+    }
+
+}
